bbf1dc6cc5d77825501cc4b2def92426579c7309
qiubo <qiubo@alddin-holdings.com>
2018-10-14 20:56:32 +0800

    origin code

---
+++ GitTestForMe/GitTestForMe.h
@@ -0,0 +1,16 @@
+//
+//  GitTestForMe.h
+//  GitTestForMe
+//
+//  Created by 邱博 on 2018/10/14.
+//  Copyright © 2018年 Aladdin-holding. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GitTestForMe : NSObject
+
+- (void)sayHelloWorld;
+- (void)showMyColor;
+
+@end
---
+++ GitTestForMe/GitTestForMe.m
@@ -0,0 +1,22 @@
+//
+//  GitTestForMe.m
+//  GitTestForMe
+//
+//  Created by 邱博 on 2018/10/14.
+//  Copyright © 2018年 Aladdin-holding. All rights reserved.
+//
+
+#import "GitTestForMe.h"
+
+@implementation GitTestForMe
+
+- (void)sayHelloWorld
+{
+    NSLog(@"Hello Wordl");
+}
+- (void)showMyColor
+{
+    NSLog(@"My favoriate color is Blue");
+}
+
+@end
